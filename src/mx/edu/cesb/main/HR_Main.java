690cf6aa20738a6acdfaee7bd4d41a25e0eefecb
Noel <Noel@DESKTOP-0FQ9JN3>
2021-03-05 22:41:49 -0600

    GUI

--- src/mx/edu/cesb/main/HR_Main.java
+++ src/mx/edu/cesb/main/HR_Main.java
@@ -1,8 +1,8 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+Noel Franciscoco Garcia Trejo
+Matricula: 2022580
+
+*/
 package mx.edu.cesb.main;
 
 import javax.swing.JOptionPane;
